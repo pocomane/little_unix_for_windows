84f654a7d27eff85e0ab0a2e94eb37f25510bef1
pocomane <pocomane@nomail.com>
2017-02-01 12:37:31 +0100

    All the env was moved in a dedicated batch file. Using ash -l and login script instead of an adhoc one. Saving ash history.

---
+++ env.bat
@@ -0,0 +1,6 @@
+:
+@echo off
+set MSYS=winsymlinks:nativestrict
+set MSYSROOT=%~dp0
+set PATH=%MSYSROOT%usr\bin;%PATH%
+set HOME=/home/user
--- terminal.bat
+++ terminal.bat
@@ -1,7 +1,6 @@
 :
 @echo off
-set MSYS=winsymlinks:nativestrict
-set MSYSROOT=%~dp0
-set ENV=/etc/env
-start %MSYSROOT%\usr\bin\mintty.exe /usr/bin/busybox.exe ash %*
+call %~dp0env.bat
+
+start %MSYSROOT%\usr\bin\mintty.exe /usr/bin/busybox.exe ash -l %*
 
